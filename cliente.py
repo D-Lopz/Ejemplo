71d1c11873cd2d2254ee2d4bb9634a6a22bde3b9
david <ddlg.email.gmail.com>
2024-11-08 17:05:58 -0500

    Prueba

--- cliente.py
+++ cliente.py
@@ -6,6 +6,8 @@ class Cliente:
 
 persona1 = Cliente("David", 18)
 persona2 = Cliente("Daniel", 18)
+persona3 = Cliente("lopez", 18)
 
 print(persona1.nombre, persona1.edad)
 print(persona2.nombre, persona2.edad)
+print(persona3.nombre, persona3.edad)
