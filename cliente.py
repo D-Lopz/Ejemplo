8f598aa8ad0511f1cd54531e251597322889b947
david <ddlg.email.gmail.com>
2024-11-08 16:49:27 -0500

    Version 3 del proyecto

--- cliente.py
+++ cliente.py
@@ -5,4 +5,7 @@ class Cliente:
         self.edad = edad
 
 persona1 = Cliente("David", 18)
+persona2 = Cliente("Daniel", 18)
+
 print(persona1.nombre, persona1.edad)
+print(persona2.nombre, persona2.edad)
