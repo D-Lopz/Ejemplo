809405bd71b6ef892844cbbcea6eb543c66282a5
david <ddlg.email.gmail.com>
2024-11-08 16:43:57 -0500

    Version 2 del proyecto

--- cliente.py
+++ cliente.py
@@ -3,3 +3,6 @@ class Cliente:
     def __init__ (self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+
+persona1 = Cliente("David", 18)
+print(persona1.nombre, persona1.edad)
